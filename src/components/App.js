ddb472061fece262f3c4e22736435c5f05283554
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-03-08 21:36:42 +0000

    Added header and styles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./Helpers/Header";
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello CybSafe</p>
+      <Header className="Header" dark={true}>
+        CybSafe Pokedex
+      </Header>
       <Router>
         <Route
           path="/"
