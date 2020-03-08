e11a81ffee49642092eb0e5240bb40979226aa59
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-03-08 21:27:00 +0000

    Implemented React Router with two routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <p>Hello CybSafe</p>
+      <Router>
+        <Route
+          path="/"
+          exact={true}
+          render={props => <pre>You're in the home page!</pre>}
+        />
+        <Route
+          path="/:pokemon"
+          render={props => (
+            <pre>
+              You're in {props.match.url} and currently looking for the pokemon
+              "{props.match.params.pokemon}".
+            </pre>
+          )}
+        />
+      </Router>
     </div>
   );
 };
